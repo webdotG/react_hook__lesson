dc8e3b040b51daeaccf3c6c840ab5202792c986c
webDotG <webdotg.inbox@gmail.com>
2023-04-30 16:39:53 +0300

    use state example components add

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import UseState from "./UseState";
 
 function App() {
 
-
-
-
   return (
     <div className="App">
       <UseState />
     </div>
   );
 }
-//на баттон выешаю обработчик при клике который будет вызщывать функцию addnumber
+
 export default App;
