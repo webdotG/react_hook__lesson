2bb9205964d244ff3cf54246256b7d2ddc5195ed
webDotG <webdotg.inbox@gmail.com>
2023-04-30 13:26:15 +0300

    base style add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@
 function App() {
   return (
     <div className="App">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+    <button>next number</button>
     </div>
   );
 }
