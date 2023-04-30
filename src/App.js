4901863f53293ea270c572230c4e3da2ff92ad17
webDotG <webdotg.inbox@gmail.com>
2023-04-30 21:27:56 +0300

    useEffect example 1/2 add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import UseState from "./UseState";
-
+import UseEffects from "./UseEffects";
 
 function App() {
 
   return (
     <div className="App">
       <UseState />
+      <UseEffects/>
     </div>
   );
 }
